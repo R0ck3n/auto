3c88fd0b482ba10a2c84fd02a823e424a87e7213
Nicolas Gracia <r0kk3nn@gmail.com>
2024-10-17 20:30:14 +0200

    push n° 1 du 17/10/2024.

--- script.sh
+++ script.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
-# Récupérer le fichier
 fichier="README.md"
-
-# Obtenir la date du jour au format d/m/Y
 date_jour=$(date +%d/%m/%Y)
+
+
+cd C:/Users/graci/Documents/auto
+
 
 # Compter les occurrences de la date dans le fichier
 occurrences=$(grep -o "$date_jour" "$fichier" | wc -l)
-pushnumber=$((occurrences + 1))
+push_number=$((occurrences + 1))
 
-# Afficher le résultat
-echo "push n° $pushnumber du $date_jour." >> $fichier
+# Edite le fichier et ajoute une ligne
+echo "push n° $push_number du $date_jour." >> $fichier
 
+# Push les modif sur gitHub
 git add .
-git commit -m "push n° $pushnumber du $date_jour."
+git commit -m "push n° $push_number du $date_jour."
 git push
+
+read -n 1
